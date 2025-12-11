1a04918947ecf8e76e58cba87ae1b7c3e4ffb0c6
William Bie Andreassen <williba@stud.ntnu.no>
2025-12-11 10:44:13 +0100

    Update Nextjs and React versions

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -60,14 +60,13 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
-        ),
+        Chevron: ({ orientation, className, ...props }) =>
+          orientation === "left" ? (
+            <ChevronLeft className={cn("size-4", className)} {...props} />
+          ) : (
+            <ChevronRight className={cn("size-4", className)} {...props} />
+          ),
       }}
-      {...props}
     />
   )
 }
